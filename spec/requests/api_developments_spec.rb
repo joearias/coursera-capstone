0259e68c6fafd37f1c812db65e1d0903fa809a2e
Joe Arias <joearias@gmail.com>
2018-01-27 17:48:25 -0600

    updated test

--- spec/requests/api_developments_spec.rb
+++ spec/requests/api_developments_spec.rb
@@ -1,15 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe "ApiDevelopments", type: :request do
+  def parsed_body
+    JSON.parse(response.body)
+  end
+
   describe "RDMS-backed" do
+    before(:each) {Foo.delete_all}
+    after(:each) {Foo.delete_all}
+
     it "create RDBMS-backed model" do
+      object=Foo.create(:name=>"test")
+      expect(Foo.find(object.id).name).to eq("test")
     end
     it "exposes RDBMS-backed API resource" do
+      object=Foo.create(:name=>"test")
+      expect(foo_path).to eq("/api/foos")
+
+      get foo_path(object.id)
+      expect(response).to have_http_status(ok)
+      expect(parsed_body["name"]).to eq("test")
     end
   end
 
   describe "MongoDB-backed" do
-    it "create MongoDB-backed model"
-    it "exposes MongoDB-backed API resource"
+    before(:each) {Bar.delete_all}
+    after(:each) {Bar.delete_all}
+
+    it "create MongoDB-backed model" do 
+      object=Bar.create(:name=>"test")
+      expect(Bar.find(object.id).name).to eq("test")
+
+    end
+    it "exposes MongoDB-backed API resource" do
+      object=Bar.create(:name=>"test")
+      expect(bar_path).to eq("/api/bars")
+
+      get bar_path(object.id)
+      expect(response).to have_http_status(ok)
+      expect(parsed_body["name"]).to eq("test")
+
+    end
   end
 end
