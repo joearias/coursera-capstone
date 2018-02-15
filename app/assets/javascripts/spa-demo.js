4a9e044b744b4cddd9a4c007df36bf0ad089d2f4
Joe Arias <joearias@gmail.com>
2018-02-15 16:12:54 -0600

    ready for spa development

---
+++ app/assets/javascripts/spa-demo.js
@@ -0,0 +1,6 @@
+// spa-demo javascripts Manifest File
+//= require jquery2
+//= require bootstrap
+//= require angular
+//= require angular-ui-router
+//= require angular-resource
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
-  # Prepend all log lines with the following tags.
+  # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
